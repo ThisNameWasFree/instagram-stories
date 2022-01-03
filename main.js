994d662ea151455f68435399a163694ad1fa238e
vitalyh <vitalyh@inuits.eu>
2022-01-03 21:17:52 +0200

    display 'previous' and 'next' screen on click

---
+++ main.js
@@ -0,0 +1,19 @@
+document.querySelector('.player-chunk-prev').addEventListener('click', function() {
+    moveClass('timeline-chunk-active', 'previousElementSibling');
+    moveClass('player-chunk-active', 'previousElementSibling');
+});
+
+document.querySelector('.player-chunk-next').addEventListener('click', function() {
+    moveClass('timeline-chunk-active', 'nextElementSibling');
+    moveClass('player-chunk-active', 'nextElementSibling');
+});
+
+function moveClass(className, method) {
+    let active = document.querySelector('.' + className);
+    let next = active[method];
+
+    if (next) {
+        active.classList.remove(className);
+        next.classList.add(className);
+    }
+}
